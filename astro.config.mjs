505625a958549892a51f4aa67f6f8b4e84d158d9
yolbert28 <yolbertdev@gmail.com>
2026-05-23 22:12:50 -0400

    uploading the love letter frontend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://love-letter-api-1o5t.onrender.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
+});
